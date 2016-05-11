f6292d26f29b34b8ff5897b10b8c35de2a0e9ced
Matthew Cheng <matthew@appx.hk>
2016-05-11 05:56:19 +0000

    fix HeaderDropdown newEnquiries filter

--- system/src/containers/header/header-dropdown-container.js
+++ system/src/containers/header/header-dropdown-container.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { connect } from 'react-redux';
-import { List } from 'immutable';
+import { Map, List } from 'immutable';
 import enquiryModule from '../../modules/enquiries';
 import HeaderDropdown from '../../components/header/header-dropdown';
 
 class HeaderDropdownContainer extends Component {
 
   static propTypes = {
+    user: React.PropTypes.instanceOf(Map),
     enquiries: React.PropTypes.instanceOf(List).isRequired
   };
 
@@ -19,7 +20,10 @@ class HeaderDropdownContainer extends Component {
 
   render() {
     return React.createElement(HeaderDropdown, {
-      newEnquiries: this.props.enquiries.filter(enquiry => enquiry.get('status') === 'New')
+      newEnquiries: this.props.enquiries.filter(enquiry => (
+        enquiry.get('status') === 'New' &&
+          enquiry.get('sales') === this.props.user.get('username')
+      ))
     });
   }
 
@@ -27,6 +31,7 @@ class HeaderDropdownContainer extends Component {
 
 function mapStateToProps(state) {
   return {
+    user: state.users.getIn(['data', 'user']),
     enquiries: state[enquiryModule.entityUrl].getIn(['listView', 'data', 'entities'])
   };
 }
